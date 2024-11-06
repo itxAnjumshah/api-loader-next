071ba93acfe53eeac3a30564c05eb668f09ab105
Anjum Shah <anjumshah402@gmail.com>
2024-11-06 14:55:06 +0500

    here  i share  aoi loader  in this  commit

---
+++ src/app/api-loader-client/page.jsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import Loader from "./loader";
+
+
+async function myList() {
+    let data=await fetch("https://jsonplaceholder.typicode.com/users")
+    data =await data.json();
+    return data;
+}
+
+const page = () => {
+
+const [users,setusers]=useState([]);
+const [loading,setloading]=useState(true);
+useEffect(()=>{
+ const fetchdata =async () =>{
+   let fethdatausers =await myList();
+   setusers(fethdatausers);
+   setloading(false)
+ }
+ fetchdata()
+},[])
+
+
+  return (
+    <>
+<h1>here  is  loading  api data </h1>    
+{
+   loading ? <Loader/> :(
+    users.map((user,i) =>(
+      <h3 key={i}>
+        name: {user.username}
+      </h3>
+    ))
+   )
+}
+
+    </>
+  )
+}
+
+export default page
